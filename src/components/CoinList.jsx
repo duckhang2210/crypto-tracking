3e9b7b8fb6755aece0d5357e3ae5c5cf27ee07b7
unknown <duckhang2210@gmail.com>
2021-06-25 01:13:29 -0500

    add styling and add search functionality

--- src/components/CoinList.jsx
+++ src/components/CoinList.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState} from 'react'
 import coinGecko from '../api/coiGecko'
+import CoinDetails from './CoinDetails'
 
 const CoinList = () => {
     const [coins, setCoins] = useState([])
+    const [search, setSearch] = useState('');
     const [isLoading, setIsLoading] = useState(false)
     useEffect( () => {
         const fetchData = async () => {
             setIsLoading(true)
             const response = await coinGecko.get('/coins/markets',{
                 params: {
-                    vs_currency: 'usd',
+                    'vs_currency': 'usd',
                     order: 'market_cap_desc',
-                    per_page: 100,
+                    'per_page': 100,
                     page: 1, 
                     sparkline: false
                 }
@@ -22,10 +24,43 @@ const CoinList = () => {
         }
         fetchData()
     },[])
+
+    const handleChange = (e) => {
+		setSearch(e.target.value);
+	};
+
+	const filterCoins = coins.filter((coin) =>
+		coin.name.toLowerCase().includes(search.toLocaleLowerCase())
+	);
+
     return (
-        <div>
-            
-        </div>
+        <div className='coin-app'>
+			<div className='coin-search'>
+				<h1 className='coin-text'>Search a Crypto</h1>
+				<form>
+					<input
+						type='text'
+						placeholder='Search'
+						className='coin-input'
+						onChange={handleChange}
+					/>
+				</form>
+			</div>
+			{filterCoins.map((coin) => {
+				return (
+					<CoinDetails
+						key={coin.id}
+						name={coin.name}
+						image={coin.image}
+						symbol={coin.symbol}
+						marketCap={coin.market_cap}
+						price={coin.current_price}
+						volume={coin.total_volume}
+						priceChange={coin.price_change_percentage_24h}
+					/>
+				);
+			})}
+		</div>
     )
 }
 
