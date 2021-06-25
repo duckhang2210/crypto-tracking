e6cb0b1af07812d0887dc882352ff9543704cf0d
unknown <duckhang2210@gmail.com>
2021-06-24 19:48:28 -0500

    create Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import CoinDetailPage from './pages/CoinDetailPage';
+import CoinSummaryPage from './pages/CoinSummaryPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<BrowserRouter>
+				<Route exact path='/' component={CoinSummaryPage} />
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default App;
